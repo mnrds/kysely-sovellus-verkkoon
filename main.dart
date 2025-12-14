2ac3fa3518a6bfb8848f764e40c670132a8eeadd
mnrds <markus.joensuu.93@gmail.com>
2025-12-14 16:13:16 +0200

    Initial commit: Kysely-sovellus verkkoon

---
+++ main.dart
@@ -0,0 +1,47 @@
+import 'dart:html';
+
+main() async {
+  var osoite = 'kysymykset.json';
+  var sisalto = await HttpRequest.getString(osoite);
+  var sanakirja = jsonDecode(sisalto);
+
+  var kysymykset = sanakirja['kysymykset'];
+
+  querySelector('#seuraava').onClick.listen((e) {
+    kysymykset.shuffle();
+    asetaKysymys(kysymykset[0]);
+  });
+}
+
+asetaKysymys(kysymys) {
+  asetaKysymysteksti(kysymys['teksti']);
+  asetaVastausvaihtoehdot(kysymys['vaihtoehdot']);
+}
+
+asetaKysymysteksti(teksti) {
+  querySelector('#kysymys').text = teksti;
+}
+
+asetaVastausvaihtoehdot(vaihtoehdot) {
+  querySelector('#vastaukset').children.clear();
+
+  for (var i = 0; i < vaihtoehdot.length; i++) {
+    lisaaVastausvaihtoehto(vaihtoehdot[i]);
+  }
+}
+
+lisaaVastausvaihtoehto(vaihtoehto) {
+  var elementti = Element.div();
+  elementti.className = 'vaihtoehto';
+  elementti.text = vaihtoehto['teksti'];
+
+  elementti.onClick.listen((e) {
+    if (vaihtoehto['oikein']) {
+      elementti.text = 'oikein!';
+    } else {
+      elementti.text = 'väärin!';
+    }
+  });
+
+  querySelector('#vastaukset').children.add(elementti);
+}
